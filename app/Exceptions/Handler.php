1cd1bd0786908cc7483e7a3443d437fdfc848e4a
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-12 11:11:43 +0700

    modified : custom error hanlder 404

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -23,8 +24,18 @@ class Handler extends ExceptionHandler
      */
     public function register(): void
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->renderable(function(NotFoundHttpException $e, $request){
+            if($request->is('api/*')){
+                return response()->json([
+                    'error' => [
+                        'message' => 'Resource not found',
+                        'type' => 'NotFoundHttpException',
+                        'code' => '4405',
+                        'link' => 'example.com/link',
+                        'status_code' => $e->getStatusCode()
+                    ]
+                ]);
+            }
         });
     }
 }

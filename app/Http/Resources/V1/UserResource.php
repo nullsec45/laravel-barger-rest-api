434458d39dce721bb056a97fb7b85e025bf10c68
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-16 16:07:20 +0700

    feat/user : get user by login.

---
+++ app/Http/Resources/V1/UserResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Support\Arr;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            $this->merge(Arr::except(parent::toArray($request),[
+                'created_at',
+                'updated_at',
+                'email',
+                'email_verified_at'
+            ]))
+        ];
+    }
+}

d3ab36f8db7f421001cd91c4316494aa70d5cc34
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-16 16:07:38 +0700

    feat/auth : returhn data & token sanctum.

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
@@ -13,13 +14,22 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): Response
+    public function store(LoginRequest $request)
     {
         $request->authenticate();
 
         $request->session()->regenerate();
 
-        return response()->noContent();
+        $user=Auth::user();
+
+        return response()->json([
+            'success' => true,
+            'data' => [
+                'token' => $user->createToken($user->name)->plainTextToken,
+                'name' => $user->name
+            ],
+            'message' => "User logged in"
+        ]);
     }
 
     /**
